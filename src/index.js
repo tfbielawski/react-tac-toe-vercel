74cf1b5bffd4ff77cc436cf0961ce1564f7b1386
tfbielawski <tfbielawski@protonmail.com>
2021-10-11 22:51:19 -0400

    initial vercel setupg

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ * Tom Bielawski
+ * React.Tac.Toe game
+ * 7/29/2021
+ * Collaborating Dev: Jesse Johns
+ * */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
